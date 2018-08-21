2669f05dad5fa8055a36701f357ca229652c7bcf
Orkhan Huseyn <orkhan.huseyn@outlook.com>
2018-08-21 11:22:00 +0400

    notes done

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 
-console.log('Starting app.js');
-
 const yargs  = require('yargs');
-
-const fs = require('fs');
-const os = require('os');
-// const _  = require('lodash');
 const notes = require('./notes.js');
-
-// const res = notes.addNote();
-//
-// console.log(_.isString(true));
-// console.log(_.isString('amiqo'));
-// console.log(_.isString(12));
-// console.log(_.uniq([1, 2, 3, 4, 5, 4]));
-
-// const user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello, ${user.username}! You are ${notes.age}.`, function(err) {
-//     if (err) throw err;
-//     console.log('Written!');
-// });
 
 const argv = yargs.argv;
 
 const command = process.argv[2];
-
-console.log('Command: ', command);
 
 if (command === 'add') {
     notes.addNote(argv.title, argv.body);
--- notes.js
+++ notes.js
@@ -1,18 +1,46 @@
 
+const fs = require('fs');
+
+const FILE_PATH = 'notes-data.json';
+
+const fetchNotes = () => {
+    const notes = fs.readFileSync(FILE_PATH);
+    return JSON.parse(notes);
+};
+
+const saveNotes = (notes) => {
+    fs.writeFileSync(FILE_PATH, JSON.stringify(notes));
+};
+
 const addNote = (title, body) => {
-    console.log('Adding note... %s %s', title, body);
+    let notes = [];
+    const note = {
+        title,
+        body
+    };
+    const notesString = fs.readFileSync(FILE_PATH);
+    notes = JSON.parse(notesString);
+    notes.push(note);
+    saveNotes(notes);
+    console.log('Note added!');
 };
 
 const getAll = () => {
-  console.log('Getting all notes...');
+  let notes = fetchNotes();
+  console.log(notes);
 };
 
 const getNote = (title) => {
-    console.log('Getting node... %s', title);
+    let notes = fetchNotes();
+    let note = notes.find(item => item.title = title);
+    console.log(note);
 };
 
 const removeNode = (title) => {
-    console.log('Removing node... %s', title);
+    let notes = fetchNotes();
+    let filteredNotes = notes.filter(item => item.title !== title);
+    saveNotes(filteredNotes);
+    console.log('Note removed!');
 };
 
 module.exports = {
